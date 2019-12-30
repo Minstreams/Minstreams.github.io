ebd1bca4498ba61dfa7f74582ea3823adf07f4ac
Minstreams <minstreams@qq.com>
2019-12-30 22:34:49 +0800

    effect adjest

--- js/IndexEffect.js
+++ js/IndexEffect.js
@@ -63,7 +63,7 @@ function Render() {
     // 背景
     var bgGradient = c2d.createLinearGradient(0, y, 0, y + window.innerHeight);
     var yRate = y / mainCanvas.height;
-    bgGradient.addColorStop(0, "rgb(" + (12 + 48 * yRate) + "," + (4 + 32 * yRate) + "," + (20 + 120 * yRate) + ")");
+    bgGradient.addColorStop(0, "rgb(" + (16 + 48 * yRate) + "," + (16 + 32 * yRate) + "," + (18 + 120 * yRate) + ")");
     bgGradient.addColorStop(1, "rgb(" + (16 + 60 * yRate) + "," + (8 + 64 * yRate) + "," + (24 + 146 * yRate) + ")");
     c2d.fillStyle = bgGradient;
     c2d.fillRect(0, 0, mainCanvas.width, mainCanvas.height);
@@ -77,17 +77,29 @@ function Render() {
     //标题
     doAnimation(y, 0, window.innerHeight * 2, anim0, function (t) {
         //背景
-        c2d.fillStyle = "rgba(" + (255) + "," + (255) + "," + (255) + "," + t * (400 - y) / 400 + ")"
+        c2d.shadowColor = "rgba(" + (215) + "," + (200) + "," + (215) + "," + (0.24) + ")";
+        c2d.shadowBlur = 180 * t;
+        var tbgGradient = c2d.createLinearGradient(0, 0, 0, window.innerHeight);
+        tbgGradient.addColorStop(0, "rgba(" + (255) + "," + (255) + "," + (215) + "," + t * (400 - y) / 400 + ")");
+        tbgGradient.addColorStop(1, "rgba(" + (230) + "," + (215) + "," + (230) + "," + t * (400 - y) / 400 + ")");
+        c2d.fillStyle = tbgGradient;
         c2d.beginPath();
         c2d.arc(mainCanvas.width / 2, window.innerHeight / (2 * t) + y * 0.7, 300, 0, 360);
         c2d.fill();
-        c2d.fillStyle = "rgba(" + (200) + "," + (200) + "," + (200) + "," + t * (400 - y) / 400 + ")"
+
+        c2d.shadowColor = "empty";
+        var tbgGradient2 = c2d.createLinearGradient(0, 0, 0, window.innerHeight);
+        tbgGradient2.addColorStop(0, "rgba(" + (190) + "," + (190) + "," + (170) + "," + t * (400 - y) / 400 + ")");
+        tbgGradient2.addColorStop(1, "rgba(" + (190) + "," + (170) + "," + (190) + "," + t * (400 - y) / 400 + ")");
+        c2d.fillStyle = tbgGradient2;
         c2d.beginPath();
         c2d.arc(mainCanvas.width / 2 + 60, window.innerHeight / (2 * t) + y * 0.7 - 80, 200, 0, 360);
         c2d.fill();
 
         //标题
         t = clamp01(t * 2 - 1);
+        c2d.shadowColor = "rgba(" + (32) + "," + (8) + "," + (40) + "," + (0.7) + ")";
+        c2d.shadowBlur = 20 * t;
         c2d.fillStyle = "rgba(" + (32) + "," + (8) + "," + (40) + "," + t * (400 - y) / 400 + ")"
         c2d.font = (60 + t * 36) + "px Cambria, Cochin, Georgia, Times, 'Times New Roman', serif"
         c2d.fillText("Minstreams'", mainCanvas.width / 2 - 234 + 80 * (1 - t), window.innerHeight / 2 + 200 * (1 - t) + y * 0.7);
